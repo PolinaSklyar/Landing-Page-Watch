87f387516be0e9cfb0829016289c6ebfba12f39a
Polina Sklyar <polinasklyar89@gmail.com>
2022-09-12 16:31:28 +0300

    lounched project

---
+++ js/index.js
@@ -0,0 +1,31 @@
+/*var bar = '';
+
+function foo (bar){
+    console.count();
+    console.count(bar)
+}
+
+foo('first');
+foo('second');
+console.countReset();
+foo('second');
+*/
+/*
+var user = "";
+
+function greet() {
+  console.count();
+  return "hi " + user;
+}
+
+user = "bob";
+greet();
+user = "alice";
+greet();
+greet();
+console.count();
+
+console.log('hello');*/
+
+var text = 'Hello, world!';
+console.log(text);
